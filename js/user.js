9b50ad1d1528c19782d5f4a90370192ba86d2358
Kevin Dick <kbdick@gmail.com>
2016-10-27 21:15:12 +0000

    Add callback

--- js/user.js
+++ js/user.js
@@ -2,13 +2,17 @@
     //data connection
     // Retrieve Data from TableTop
     /* global Tabletop */
-var currentAmount = document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
   		Tabletop.init( { 
   		key: "1OVgXVxrfM13WwwjqBC4jlYlzTQuSrHrn43r5T0KWbjY", 
-  		callback: function(data, tabletop) { console.log( "Here is the Audubon Thermometer Data", data); return currentAmount.data[0].dollarsraised;}, 
+  		callback: showInfo,
   		parseNumbers: true,
   		simpleSheet: true 
   		});
 });
 
-
+function showInfo(data, tabletop) { 
+    console.log( "Here is the Audubon Thermometer Data", data); 
+}
+/* global tabletop */
+var currentAmount = tabletop.data[0].dollarsraised;

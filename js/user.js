affa580950cac6ea24e11e7f95cd2f2c91535a11
Kevin Dick <kbdick@gmail.com>
2016-10-27 19:40:45 +0000

    Trying variable as function

--- js/user.js
+++ js/user.js
@@ -2,14 +2,12 @@
     //data connection
     // Retrieve Data from TableTop
     /* global Tabletop */
-document.addEventListener('DOMContentLoaded', function() {
+var currentAmount = document.addEventListener('DOMContentLoaded', function() {
   		Tabletop.init( { 
   		key: "1OVgXVxrfM13WwwjqBC4jlYlzTQuSrHrn43r5T0KWbjY", 
   		callback: function(data, tabletop) { console.log( "Here is the Audubon Thermometer Data", data) }, 
   		parseNumbers: true,
   		simpleSheet: true 
   		});
-  		
-var currentAmount = data[0].dollarsraised;
-    
+  		return data[0].dollarsraised;
 });
